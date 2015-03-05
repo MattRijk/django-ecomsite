e9d0515a34d301d285060d1d90657c5557bac26c
Matt Rijk <matthewrijk@gmail.com>
2015-03-05 14:04:53 -0500

    added products app to site

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'ecomsite.views.home', name='home'),
+    url(r'^$', 'products.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
