6c671bd9f78ec503da03eaa7f8c4f9a793543d2c
Matt Rijk <matthewrijk@gmail.com>
2015-03-05 15:18:29 -0500

    added to app and added interface with bootstrap

--- ecomsite/products/views.py
+++ ecomsite/products/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from lib2to3.fixes.fix_input import context
 
 
 # Create your views here.
 def home(request):
     
-    context = locals()
-    template = 'home.html'
+    if request.user.is_authenticated():
+        username_is = "Matt using context"
+        context = {"username_is": request.user.email}   # or just request.user
+    else:
+        context = {"username_is":"unknown"}
+    
+    
+    template = 'base.html'
     return render(request, template, context)
