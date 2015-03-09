bedd900ee6c3cd86687e337c351abd1295d54fc0
Matt Rijk <matthewrijk@gmail.com>
2015-03-09 13:25:59 -0400

    added static files

--- ecomsite/ecomsite/settings.py
+++ ecomsite/ecomsite/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from django.conf.global_settings import MEDIA_URL, STATICFILES_DIRS
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -82,6 +83,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+#MEDIA_ROOT = '/home/matt/Documents/ecomsite/static/media/'
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "static_root")
+
+STATICFILES_DIRS = (
+        os.path.join(os.path.dirname(BASE_DIR), "static", "static_files"),
+        )
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
