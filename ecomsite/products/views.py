4962089d12dadf15b4c7fbd066bd4c2371f48510
Matt Rijk <matthewrijk@gmail.com>
2015-03-09 10:07:05 -0400

    updated view

--- ecomsite/products/views.py
+++ ecomsite/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from lib2to3.fixes.fix_input import context
-
+from .models import Product
 
 # Create your views here.
 def home(request):
@@ -9,8 +9,19 @@ def home(request):
         username_is = "Matt using context"
         context = {"username_is": request.user}   # or just request.user
     else:
-        context = {"username_is":"unknown"}
-    
-    
+        context = {"username_is": request.user}
+        
     template = 'products/home.html'
     return render(request, template, context)
+    
+    
+def all(request):
+    
+    products = Product.objects.all()
+    context = {'products': products}
+    template = 'products/all.html'
+    return render(request, template, context)
+
+
+    
+    
